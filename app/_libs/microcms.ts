c7a9d7ba59106e779c1498eb7c4b4ddf08fabc46
hisaaashi <jyake312@gmail.com>
2024-07-18 01:32:17 +0900

    ６章まで完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -20,7 +20,7 @@ export type News = {
   title: string;
   description: string;
   content: string;
-  thumbnail: MicroCMSImage;
+  thumbnail?: MicroCMSImage;
   category: Category;
 } & MicroCMSListContent;
 
@@ -51,4 +51,17 @@ export const getNewsList = async (queries?: MicroCMSQueries) => {
     queries,
   });
   return listData;
-}
+};
+
+export const getNewsDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  const detailData = await client.getListDetail<News>({
+    endpoint: "news",
+    contentId,
+    queries,
+  });
+
+  return detailData;
+};
